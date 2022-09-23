3c098aa8a815bdfbc89aab09bb2e894fdb85b71a
MatheusCardosoc0 <matheuscardosoc905@gmail.com>
2022-09-23 19:18:48 -0300

    listando filmes na aplicação

--- filmCacheWeb/src/pages/main.tsx
+++ filmCacheWeb/src/pages/main.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 import { Header } from "../components/header"
 import { api } from "../services/api"
 
@@ -12,7 +13,7 @@ interface DataFilmAtributes {
   id: number,
   original_language: string,
   original_title: string,
-  overview: string ,
+  overview: string,
   popularity: number,
   poster_path: string,
   release_date: string,
@@ -26,24 +27,37 @@ export const Main = () => {
 
   const [dataFilm, setDataFilm] = useState<DataFilmAtributes[]>([])
 
-  useEffect(() =>{
-    async function GetFilms(){
+  useEffect(() => {
+    async function GetFilms() {
       const response = await api.get("movie/now_playing", {
-        params:{
+        params: {
           api_key: "ccbfbc8dd52c71d7f1a46e1b46db20ee",
           language: "pt-br",
-          page:1,
+          page: 1,
         }
       })
-      console.log(response)
+      setDataFilm(response.data.results.slice(0, 18))
     }
     GetFilms()
-  },[])
+  }, [])
 
 
   return (
-    <div>
+    <main>
       <Header />
-    </div>
+      <div className="flex justify-center flex-col items-center gap-10 pt-20 bg-gradient-to-tr from-blue-700 via-slate-700 to-slate-900 pb-40">
+        {dataFilm.map(film => {
+          return (
+            <article key={film.id} className="text-slate-300 flex flex-col bg-gradient-to-tl from-teal-500 to-black rounded-lg overflow-hidden border-l-4 border-l-blue-500 border-t-blue-600 border-t-4 md:border-t-8 md:border-l-8">
+              <img className="w-[20rem] h-[15rem] md:w-[32rem] md:h-[20rem]  object-cover rounded-b-lg" src={`https://image.tmdb.org/t/p/w500/${film.poster_path}`} />
+              <div className="flex flex-col p-3 gap-4">
+                <strong className="md:text-3xl">{film.title}</strong>
+                <Link className="font-bold mx-auto py-1 px-3 rounded-lg bg-blue-600 md:text-2xl" to={`/film/${film.id}`} >Acessar</Link>
+              </div>
+            </article>
+          )
+        })}
+      </div>
+    </main>
   )
 }
